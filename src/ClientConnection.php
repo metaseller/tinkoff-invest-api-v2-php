781fa4085d12a6e9c5a939aa8d07e085fdbe20f1
Igor V. Frolenkov <igor@frolenkov.ru>
2022-01-24 23:19:04 +0700

    ClientConnection model

---
+++ src/ClientConnection.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Metaseller\TinkoffInvestApi2;
+
+use Grpc\ChannelCredentials;
+
+/**
+ * Модель получения настроек соединения с сервисом Tinkoff Invest API 2
+ *
+ * @package Metaseller\TinkoffInvestApi2
+ */
+class ClientConnection
+{
+    /**
+     * @var string Адрес домена сервиса Tinkoff Invest API 2
+     *
+     * @see https://tinkoff.github.io/investAPI/
+     */
+    public const TINKOFF_INVEST_API2_HOSTNAME = 'invest-public-api.tinkoff.ru';
+
+    /**
+     * @var int Порт сервиса Tinkoff Invest API 2
+     *
+     * @see https://tinkoff.github.io/investAPI/
+     */
+    public const TINKOFF_INVEST_API2_PORT = 443;
+
+    /**
+     * Метод получения опций соединения с сервисом Tinkoff Invest API 2
+     *
+     * @param string $api_token Токен доступа к Tinkoff Invest API 2
+     *
+     * @return array Массив опций соединения с сервисом Tinkoff Invest API 2
+     */
+    public static function getOptions(string $api_token): array
+    {
+        $certificate_path = dirname(__FILE__) . '/../../etc/tinkoff-ru.pem';
+
+        $cert = file_get_contents($certificate_path);
+
+        return [
+            'credentials' => ChannelCredentials::createSsl($cert),
+            'grpc.enable_http_proxy' => 0,
+            'grpc.ssl_target_name_override' => static::TINKOFF_INVEST_API2_HOSTNAME,
+            'update_metadata' => function($meta_data) use ($api_token) {
+                $meta_data['authorization'] = ['Bearer ' . $api_token];
+
+                return $meta_data;
+            }
+        ];
+    }
+
+    /**
+     * Метод получения полного адреса сервиса Tinkoff Invest API 2
+     *
+     * @return string Полного адреса сервиса Tinkoff Invest API 2
+     */
+    public static function getHostname(): string
+    {
+        return static::TINKOFF_INVEST_API2_HOSTNAME . ':' . static::TINKOFF_INVEST_API2_PORT;
+    }
+}
