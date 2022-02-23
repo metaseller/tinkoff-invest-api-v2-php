3b1b2cee50670ec23182361cfa7b6541681f5d9c
Igor V. Frolenkov <igor@frolenkov.ru>
2022-02-23 19:42:26 +0700

    Add SDK exceptions

---
+++ src/exceptions/BaseSDKException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Metaseller\TinkoffInvestApi2\exceptions;
+
+use Exception;
+
+/**
+ * Базовая модель исключения, возникшего в логике SDK
+ *
+ * @package Metaseller\TinkoffInvestApi2
+ */
+class BaseSDKException extends Exception
+{
+}
---
+++ src/exceptions/InstrumentNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Metaseller\TinkoffInvestApi2\exceptions;
+
+use Exception;
+
+/**
+ * Модель исключения 'Инструмент не найден'
+ *
+ * @package Metaseller\TinkoffInvestApi2
+ */
+class InstrumentNotFoundException extends BaseSDKException
+{
+}
